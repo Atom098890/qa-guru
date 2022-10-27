eaf542e202c967fc408d5a6eac7ea3d5d9315e93
Andrey <atom.098890@gmail.com>
2022-10-27 22:38:20 +0300

    New test dragAndDrop

---
+++ src/test/java/com/demoqa/selenide/github/SeleniumTest.java
@@ -0,0 +1,29 @@
+package com.demoqa.selenide.github;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SeleniumTest {
+
+    @Test
+    void contributeTest(){
+        open("https://github.com/selenide/selenide");
+        $(".Layout-sidebar").$(byText("Contributors"))
+                .ancestor(".BorderGrid-cell")
+                .$$("ul li").first()
+                .hover();
+
+        $$(".Popover span").first().shouldHave(text("Andrei Solntsev"));
+    }
+
+    @Test
+    void dragAndDropTest(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldNotHave(text("A"));
+    }
+}
